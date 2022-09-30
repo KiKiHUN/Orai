67df0e8216e3747cb325f7c341ab2e0fd436a6be
KIKIHUN <kikimano001@gmail.com>
2022-09-30 10:30:54 +0200

    bf

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
    public function show($slug)
    {
         //return view ('post', compact('post'));
-        $post=\DB::table('posts')->where('slug',$slug)->first();
+        $post=DB::table('posts')->where('slug',$slug)->first();
         //$s=DB::select('Select * from posts');
         //dd($s);
         return view ('post', compact('post'));
